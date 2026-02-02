5c704f0ec0f2df9995ed67f09d385b48b58b00bc
n-mariana <mariana.sgoncalves2010@gmail.com>
2026-02-02 15:55:17 -0300

    Add Migrations

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -1,7 +1,15 @@
 package br.com.dio;
 
+import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
+
+import java.sql.SQLException;
+
+import br.com.dio.persistence.migration.MigrationStrategy;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        try(var connection = getConnection()){
+                new MigrationStrategy(connection).executeMigration();
+            }
+        }
     }
-}
